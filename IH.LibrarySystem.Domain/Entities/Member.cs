5be36d040bf429aaf3574cdedd216c7598c762ed
Tomás Bernal <tbernalz@eafit.edu.co>
2026-01-01 17:49:55 +0100

    feat(domain): add Member entity and creation logic

---
+++ IH.LibrarySystem.Domain/Entities/Member.cs
@@ -0,0 +1,30 @@
+using IH.LibrarySystem.Domain.Enums;
+using StandUsers.Domain.SharedKernel;
+
+namespace IH.LibrarySystem.Domain.Entities;
+
+public class Member : Entity
+{
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+    public DateTime JoinDate { get; private set; }
+    public MemberStatus Status { get; private set; }
+
+    private Member()
+        : base(Guid.Empty) { }
+
+    private Member(Guid id, string name, string email, DateTime? joinDate = null)
+        : base(id)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        Name = name;
+        Email = email;
+        JoinDate = joinDate ?? DateTime.UtcNow;
+        Status = MemberStatus.Active;
+    }
+
+    public static Member Create(Guid id, string name, string email, DateTime? joinDate = null) =>
+        new Member(id, name, email, joinDate);
+}
