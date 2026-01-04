c166acb8b0ac10a14eb72b287c5105942cda02e4
Tomás Bernal <tbernalz@eafit.edu.co>
2026-01-04 12:48:35 +0100

    chore(domain): remove duplicate singular directories

--- IH.LibrarySystem.Domain/Book/BookStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace IH.LibrarySystem.Domain.Enums;
-
-public enum BookStatus
-{
-    Available = 1,
-    Loaned = 2,
-    Lost = 3,
-    Archived = 4,
-}
--- IH.LibrarySystem.Domain/Member/MemberStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace IH.LibrarySystem.Domain.Enums;
-
-public enum MemberStatus
-{
-    Active = 1,
-    Suspended = 2,
-    Canceled = 3,
-}
