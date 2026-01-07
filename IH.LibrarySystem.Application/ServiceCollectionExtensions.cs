04a3ee3973edf4e876f5f215273b39649665d3d1
Tomás Bernal <tbernalz@eafit.edu.co>
2026-01-07 15:49:14 +0100

    chore(application): register application services in DI container

---
+++ IH.LibrarySystem.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using IH.LibrarySystem.Application.Authors;
+using IH.LibrarySystem.Application.Books;
+using IH.LibrarySystem.Application.Loans;
+using IH.LibrarySystem.Application.Members;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IH.LibrarySystem.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddScoped<IAuthorService, AuthorService>();
+        services.AddScoped<IBookService, BookService>();
+        services.AddScoped<IMemberService, MemberService>();
+        services.AddScoped<ILoanService, LoanService>();
+
+        return services;
+    }
+}
