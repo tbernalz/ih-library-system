09ba4f7822bdb0d364aeddff37f06e48bd5c50ea
Tomás Bernal <tbernalz@eafit.edu.co>
2026-01-09 20:30:47 +0100

    feat(infrastructure): add generic base repository implementation

---
+++ IH.LibrarySystem.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+using IH.LibrarySystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IH.LibrarySystem.Infrastructure.Repositories;
+
+public abstract class BaseRepository<T>
+    where T : class
+{
+    protected readonly LibraryDbContext Context;
+    protected readonly DbSet<T> DbSet;
+
+    protected BaseRepository(LibraryDbContext context)
+    {
+        Context = context;
+        DbSet = context.Set<T>();
+    }
+
+    public virtual async Task<T?> GetByIdAsync(Guid id) => await DbSet.FindAsync(id);
+
+    public virtual async Task AddAsync(T entity) => await DbSet.AddAsync(entity);
+
+    public virtual void Update(T entity) => DbSet.Update(entity);
+
+    public virtual void Delete(T entity) => DbSet.Remove(entity);
+
+    public virtual async Task SaveChangesAsync() => await Context.SaveChangesAsync();
+}
