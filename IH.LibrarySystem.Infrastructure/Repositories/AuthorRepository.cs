bc37a218f763706b5ac1ae392a0c40c8632a6d6f
Tomás Bernal <tbernalz@eafit.edu.co>
2026-01-12 14:26:05 +0100

    feat(infrastructure): implement AuthorRepository

---
+++ IH.LibrarySystem.Infrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,17 @@
+using IH.LibrarySystem.Domain.Authors;
+using IH.LibrarySystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IH.LibrarySystem.Infrastructure.Repositories;
+
+public class AuthorRepository : BaseRepository<Author>, IAuthorRepository
+{
+    public AuthorRepository(LibraryDbContext context)
+        : base(context) { }
+
+    public async Task<Author?> GetByEmailAsync(string email)
+    {
+        var lowerEmail = email.ToLower();
+        return await DbSet.FirstOrDefaultAsync(a => a.Email.ToLower() == lowerEmail);
+    }
+}
