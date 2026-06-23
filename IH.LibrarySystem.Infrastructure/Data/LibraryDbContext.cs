a50bd4121812805cb83f7e6ff0550a37ff73d027
Tomás Bernal <tbernalz@eafit.edu.co>
2026-06-23 09:28:53 -0500

    feat(infrastructure): expose Users and RefreshTokens DbSets in LibraryDbContext

--- IH.LibrarySystem.Infrastructure/Data/LibraryDbContext.cs
+++ IH.LibrarySystem.Infrastructure/Data/LibraryDbContext.cs
@@ -1,5 +1,6 @@
 using IH.LibrarySystem.Domain.Authors;
 using IH.LibrarySystem.Domain.Books;
+using IH.LibrarySystem.Domain.Identity;
 using IH.LibrarySystem.Domain.Loans;
 using IH.LibrarySystem.Domain.Members;
 using IH.LibrarySystem.Domain.Notifications;
@@ -17,6 +18,8 @@ public class LibraryDbContext : DbContext
     public DbSet<Member> Members => Set<Member>();
     public DbSet<Loan> Loans => Set<Loan>();
     public DbSet<NotificationLog> NotificationLogs => Set<NotificationLog>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- IH.LibrarySystem.Infrastructure/Migrations/20260622211232_AddUserAndRefreshToken.Designer.cs
+++ IH.LibrarySystem.Infrastructure/Migrations/20260622211232_AddUserAndRefreshToken.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IH.LibrarySystem.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pgvector;
@@ -12,9 +13,11 @@ using Pgvector;
 namespace IH.LibrarySystem.Infrastructure.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260622211232_AddUserAndRefreshToken")]
+    partial class AddUserAndRefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
